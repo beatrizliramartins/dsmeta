144dc122b85619e106c4f5106d7df64a4772b1d4
beatriz lira <blmartins017@gmail.com>
2022-09-05 19:38:17 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import Header from "./components/Header";
 import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Olá, Mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <SalesCard />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+        </section>
+      </main>
     </>
   );
 }
